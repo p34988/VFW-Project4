86989d010e55d7bdebfc894ff9b574d991267a8e
Warren Morris <robbymorris@fullsail.edu>
2013-03-19 23:50:06 -0700

    saving

--- js/json.js
+++ js/json.js
@@ -3,3 +3,40 @@
 	VFW 1303
 	Project 4
 */
+
+var json = {
+	
+	"bike1": {
+		"assembler": ["Assembler:","Robby"],
+		"date": ["Date:","2013-03-19"],
+		"type": ["Type:","Cruiser"],
+		"marked": ["Marked:","yes"],
+		"time": ["Time:","11"],
+		"comments": ["Comments:","Junky Huffy."]
+	},
+	"bike1": {
+		"assembler": ["Assembler:","Eric"],
+		"date": ["Date:","2013-03-18"],
+		"type": ["Type:","BMX"],
+		"marked": ["Marked:","no"],
+		"time": ["Time:","23"],
+		"comments": ["Comments:","I used to use pegs to transport friends."]
+	},
+	"bike1": {
+		"assembler": ["Assembler:","Ed"],
+		"date": ["Date:","2013-03-01"],
+		"type": ["Type:","Road Bike"],
+		"marked": ["Marked:","no"],
+		"time": ["Time:","43"],
+		"comments": ["Comments:","90 psi in these tires is crazy."]
+	},
+	"bike1": {
+		"assembler": ["Assembler:","Terry"],
+		"date": ["Date:","2010-03-12"],
+		"type": ["Type:","Mountain Bike"],
+		"marked": ["Marked:","no"],
+		"time": ["Time:","360"],
+		"comments": ["Comments:","I'm not sure how to use a wrench."]
+	}
+	
+}
